2fcf6736997d8fb8760ecf6b7644415f5fb7a30c
yassyolo <yoana.yotova03@gmail.com>
2025-03-22 15:51:58 +0200

    Update output message in Program.cs (branch: master)

--- Version_Control_And_Git/Assesment1/Program.cs
+++ Version_Control_And_Git/Assesment1/Program.cs
@@ -4,5 +4,5 @@ var person = new Person();
 person.Name = "Konstantin";
 person.Age = 22;
 
-Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+Console.WriteLine($"Hello I am {person.Name} and in May I turned {person.Age}. I am currently studying physiotherapy.");
 
