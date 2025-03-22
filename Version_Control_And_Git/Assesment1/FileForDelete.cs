8477bcdc1586a34e1f9267a6383e330418d08732
yassyolo <yoana.yotova03@gmail.com>
2025-03-22 16:10:23 +0200

    Add Message property and constructor to FileForDelete (branch:delete-conflict)

--- Version_Control_And_Git/Assesment1/FileForDelete.cs
+++ Version_Control_And_Git/Assesment1/FileForDelete.cs
@@ -2,5 +2,10 @@
 {
     internal class FileForDelete
     {
+        public string Message { get; set; } = string.Empty;
+        public FileForDelete(string message)
+        {
+            Message = message;
+        }
     }
 }
