2949caf6d13971108adabca5ba8ac24149cf841e
yassyolo <yoana.yotova03@gmail.com>
2025-03-30 16:28:26 +0300

    Add product entity

---
+++ Advanced_LINQ/Assessment9/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Assessment9.Entities
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string UniqueCode { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}/{UniqueCode} - ${Price}";
+        }
+    }
+}
