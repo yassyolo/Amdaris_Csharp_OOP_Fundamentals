489bd030d6567d0e4dbf76f4d0a0779a0c97e6a4
yassyolo <yoana.yotova03@gmail.com>
2025-03-30 13:24:35 +0300

    Add StringExtensions class to manipulate string input and update the console output

---
+++ Assignments/Module1-Lesson5/StringAssignment/StringExtensions.cs
@@ -0,0 +1,53 @@
+namespace StringAssignment
+{
+	public static class StringExtensions
+	{
+		public static int CountWordsInString(this string input)
+		{
+			return input.Split(new[] { ' ', '\t', '\n', '\r', '.', ',', ';', '!', '?', ':', ';', '(', ')', '"', '[', ']', '-', '_', '/', '\\' }, StringSplitOptions.RemoveEmptyEntries).Length;
+		}
+
+		public static int CountSentences(this string input)
+		{
+			return input.Split(new[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+		}
+
+		public static string ReverseWordsInString(this string input)
+		{
+			var reversedWords = new string[input.Length];
+
+			var inputAsArray = input.Split(new[] { ' ', '\t', '\n', '\r', '.', ',', ';', '!', '?', ':', ';', '(', ')', '"', '[', ']', '-', '_', '/', '\\' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+			for (int i = 0; i < inputAsArray.Length; i++)
+			{
+				reversedWords[i] = inputAsArray[inputAsArray.Length - 1 - i];
+			}
+			return string.Join(" ", reversedWords);
+		}
+
+		public static string ReverseCharactersInString(this string input)
+		{
+			var reversedCharacters = new char[input.Length];
+			for (int i = 0; i < input.Length; i++)
+			{
+				reversedCharacters[i] = input[input.Length - 1 - i];
+			}
+			return new string(reversedCharacters);
+		}
+
+		public static int CountOccurrences(this string input, string word)
+		{
+			int ocurrences = 0;
+			int nextWordIndex = 0;
+			while (nextWordIndex < input.Length)
+			{
+				nextWordIndex = input.IndexOf(word, nextWordIndex, StringComparison.OrdinalIgnoreCase);
+				if (nextWordIndex == -1) break;
+
+				ocurrences++;
+				nextWordIndex += word.Length;
+			}
+
+			return ocurrences;
+		}
+	}
+}
