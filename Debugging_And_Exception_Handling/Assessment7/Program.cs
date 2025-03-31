34d40bafecfe19be0b22446ea90a37a51b661435
yassyolo <yoana.yotova03@gmail.com>
2025-04-01 02:06:10 +0300

    Test the car and output to the console

---
+++ Debugging_And_Exception_Handling/Assessment7/Program.cs
@@ -0,0 +1,49 @@
+﻿using Assessment7.Entities;
+using Assessment7.Exceptions;
+
+Car myCar = new Car();
+bool needsMaintenance = true;
+
+#if DEBUG
+Console.WriteLine("Lets ride!!");
+#endif
+
+try
+{
+    Console.Write("Enter speed: ");
+    int speed = int.Parse(Console.ReadLine());
+    myCar.Speed = speed;
+
+    myCar.Drive();
+
+    if (needsMaintenance)
+    {
+        myCar.Maintenance(true, true); 
+    }
+
+    myCar.Drive();
+}
+catch (TooFastException ex)
+{
+    Console.WriteLine($"{typeof(TooFastException)} -> {ex.Message}");
+}
+catch (OutOfFuelException ex)
+{
+    Console.WriteLine($"{typeof(OutOfFuelException)} -> {ex.Message}");
+    throw;  
+}
+catch (EngineFailureException ex)
+{
+    Console.WriteLine($"{typeof(EngineFailureException)} -> {ex.Message}");
+    throw;  
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Something went wrong");
+    throw;  
+}
+finally
+{
+    Console.WriteLine("Have a safe drive!");
+}
+        
