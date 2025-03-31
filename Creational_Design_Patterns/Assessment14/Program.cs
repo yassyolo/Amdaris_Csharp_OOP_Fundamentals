e6958fa9473a117e59d2c501200f7c0389baaa5c
yassyolo <yoana.yotova03@gmail.com>
2025-04-01 01:21:32 +0300

    Test the factories in the console

---
+++ Creational_Design_Patterns/Assessment14/Program.cs
@@ -0,0 +1,25 @@
+﻿using Assessment14;
+using Assessment14.Factories;
+using Assessment14.Models;
+
+CoffeeFactory espressoFactory = new EspressoFactory();
+CoffeeFactory cappuccinoFactory = new CappuccinoFactory();
+CoffeeFactory flatWhiteFactory = new FlatWhiteFactory();
+MilkFactory oatMilkFactory = new OatMilkFactory();
+MilkFactory regularMilkFactory = new RegularMilkFactory();
+MilkFactory soyMilkFactory = new SoyMilkFactory();
+
+Coffee espresso = espressoFactory.MakeCoffee();
+espresso.AddIngredient("Sugar");
+espresso.AddIngredient(soyMilkFactory.UseMilk().Name);
+Console.WriteLine(espresso.ToString());
+
+Coffee cappuccino = cappuccinoFactory.MakeCoffee();
+cappuccino.AddIngredient("Sugar");
+Console.WriteLine(cappuccino.ToString());
+
+Coffee flatWhite = flatWhiteFactory.MakeCoffee();
+flatWhite.AddIngredient("Sugar");
+flatWhite.AddIngredient("Sugar");
+
+Console.WriteLine(flatWhite.ToString());
