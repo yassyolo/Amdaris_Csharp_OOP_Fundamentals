03334fc64d7a0a7b2a9d78d45704ba9f6f9960fc
yassyolo <yoana.yotova03@gmail.com>
2025-04-05 16:13:50 +0300

    Add a facade for sending and logging the email

---
+++ Disposal_And_Garbage_Collection/Assessment10/Facade/LogAndSendEmailFacade.cs
@@ -0,0 +1,23 @@
+﻿using Assessment10.Email;
+using Assessment10.Loggers;
+
+namespace Assessment10.Facade
+{
+    internal class LogAndSendEmailFacade
+    {
+        private List<ILogger> _loggers;
+        private IEmailService _emailService;
+
+        public LogAndSendEmailFacade(List<ILogger> loggers, IEmailService emailService)
+        {
+            _loggers = loggers;
+            _emailService = emailService;
+        }
+
+        public void LogAndSendEmail(string message, string to, string subject)
+        {
+            _loggers.ForEach(logger => logger.Log(message));
+            _emailService.SendEmail(to, subject);
+        }
+    }
+}
