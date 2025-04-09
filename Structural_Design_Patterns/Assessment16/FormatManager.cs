edd3bbb15b359989f585a1d89026233e8307b1d1
yassyolo <yoana.yotova03@gmail.com>
2025-04-09 21:12:30 +0300

    Add format manager to add and remove fomatters on text

---
+++ Structural_Design_Patterns/Assessment16/FormatManager.cs
@@ -0,0 +1,43 @@
+﻿using Assessment16.Contacts;
+using Assessment16.Decorator;
+using Assessment16.Implementations;
+using System.Runtime.Serialization;
+
+namespace Assessment16
+{
+    internal class FormatManager
+    {
+        private readonly string _inputText;
+        private readonly Stack<BaseTextFormatterDecorator> _textFormatters = new();
+
+        public FormatManager(string inputText)
+        {
+            _inputText = inputText;
+        }
+
+        public void AddTextFormatter(BaseTextFormatterDecorator textFormatter)
+        {
+            _textFormatters.Push(textFormatter);
+        }
+
+        public void RemoveTextFormatter()
+        {
+            if (_textFormatters.Count > 0)
+            {
+                _textFormatters.Pop();
+            }
+        }
+
+        public string FormatText()
+        {
+            string formattedText = _inputText;
+
+            foreach (var formatter in _textFormatters)
+            {
+                formattedText = formatter.Format(formattedText);
+            }
+
+            return formattedText;            
+        }
+    }
+}
