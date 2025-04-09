ab2ce4a33c31b363546457bee8c285dde9c75a34
yassyolo <yoana.yotova03@gmail.com>
2025-04-09 21:30:34 +0300

    Add notification manager who manages subscribers and notifies them

---
+++ SOLID_Principles/Assessment12/NotificationManager/INotificationManager.cs
@@ -0,0 +1,11 @@
+﻿using Assessment12.NotificationSubscriber;
+
+namespace Assessment12.NotificationManager
+{
+    internal interface INotificationManager
+    {
+        void Subscribe(INotificationSubscriber subscriber);
+        void Unsubscribe(INotificationSubscriber subscriber);
+        void Notify(string message);
+    }
+}
---
+++ SOLID_Principles/Assessment12/NotificationManager/NotificationManager.cs
@@ -0,0 +1,27 @@
+﻿using Assessment12.NotificationSubscriber;
+
+namespace Assessment12.NotificationManager
+{
+    internal class NotificationManager : INotificationManager
+    {
+        private readonly List<INotificationSubscriber> _subscribers = new List<INotificationSubscriber>();
+
+        public void Subscribe(INotificationSubscriber subscriber)
+        {
+            _subscribers.Add(subscriber);
+        }
+
+        public void Unsubscribe(INotificationSubscriber subscriber)
+        {
+            _subscribers.Remove(subscriber);
+        }
+
+        public void Notify(string message)
+        {
+            foreach (var subscriber in _subscribers)
+            {
+                subscriber.Update(message);
+            }
+        }
+    }
+}
