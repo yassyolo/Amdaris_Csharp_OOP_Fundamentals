0e7a3ec8601ca3761cc28a2c6d5dac16ea4d1f57
yassyolo <yoana.yotova03@gmail.com>
2025-04-09 21:33:42 +0300

    Add notification service manager who registers the notification services and calls the manager to notify all subscribers

---
+++ SOLID_Principles/Assessment12/NotificationServiceManager.cs
@@ -0,0 +1,33 @@
+﻿
+using Assessment12.NotificationManager;
+using Assessment12.NotificationService;
+
+namespace Assessment12
+{
+    internal class NotificationServiceManager
+    {
+        private readonly INotificationManager _notificationManager;
+        private readonly List<INotificationService> _notificationServices = new();
+
+        public NotificationServiceManager(INotificationManager notificationManager)
+        {
+            _notificationManager = notificationManager;
+        }
+
+        public void RegisterService(INotificationService service)
+        {
+            _notificationServices.Add(service);
+        }
+
+        public void SendNotification(string to, string message)
+        {
+
+            foreach (var service in _notificationServices)
+            {
+                service.SendNotification(to, message);
+            }
+
+            _notificationManager.Notify(message);
+        }
+    }
+}
---
+++ SOLID_Principles/Assessment12/Program.cs
@@ -0,0 +1,23 @@
+﻿using Assessment12;
+using Assessment12.NotificationManager;
+using Assessment12.NotificationService;
+using Assessment12.NotificationSubscriber;
+
+var notificationManager = new NotificationManager();
+
+var user1 = new User("Yoana");
+var user2 = new User("Mariela");
+notificationManager.Subscribe(user1);
+notificationManager.Subscribe(user2);
+
+var emailService = new EmailNotificationService();
+var smsService = new SMSNotificationService();
+var pushService = new PushNotificationService();
+
+var notificationServiceManager = new NotificationServiceManager(notificationManager);
+notificationServiceManager.RegisterService(emailService);
+notificationServiceManager.RegisterService(smsService);
+notificationServiceManager.RegisterService(pushService);
+
+notificationServiceManager.SendNotification((user1.Name), "Welcome to the messaging app!");
+notificationServiceManager.SendNotification((user2.Name), "Welcome to the messaging app!");
