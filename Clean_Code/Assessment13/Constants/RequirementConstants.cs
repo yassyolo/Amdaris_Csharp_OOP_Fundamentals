229d5f16960b3907c8bcdfe2e9307d9f35519a1a
Yoana Yotova <yoana.yotova03@gmail.com>
2025-05-05 23:05:38 +0300

    Add constants instead of hardcoding values

---
+++ Clean_Code/Assessment13/Constants/RequirementConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Assessment13.Constants;
+
+public static class RequirementConstants
+{
+    public const int MinimumExperience = 10;
+    public const int CertificationCount = 3;
+    public const int MinimumBrowserMajorVersion = 9;
+}
